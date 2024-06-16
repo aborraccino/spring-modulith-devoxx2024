791ad116554a2740f36676bf309abfbaf42008f1
Alessandro Borraccino <alessandro.borraccino@worldpay.com>
2024-06-16 17:56:30 +0100

    ex 1: return the number of items currently in stock from GetProductApi

--- src/main/java/victor/training/modulith/inventory/InventoryInternalApi.java
+++ src/main/java/victor/training/modulith/inventory/InventoryInternalApi.java
@@ -22,4 +22,8 @@ public class InventoryInternalApi {
     stockService.confirmReservation(orderId);
   }
 
+  public int getStock(long productID){
+    return stockRepo.findByProductId(productID).orElseThrow().items();
+  }
+
 }
--- src/test/java/victor/training/modulith/GetProductApiTest.java
+++ src/test/java/victor/training/modulith/GetProductApiTest.java
@@ -1,7 +1,6 @@
 package victor.training.modulith;
 
 import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +13,6 @@ import victor.training.modulith.inventory.model.Stock;
 import victor.training.modulith.inventory.repo.StockRepo;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,7 +28,6 @@ public class GetProductApiTest {
   StockRepo stockRepo;
 
   @Test
-  @Disabled // TODO fix
   void returnsStock() throws Exception {
     Long productId = productRepo.save(new Product()).id();
     stockRepo.save(new Stock().productId(productId).add(5));
